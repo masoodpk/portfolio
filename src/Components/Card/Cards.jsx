5629852cb2df1d783760b878ec958964f18c95f4
masood <114647121+masoodpk@users.noreply.github.com>
2024-03-10 11:32:52 +0530

    Update Cards.jsx

--- src/Components/Card/Cards.jsx
+++ src/Components/Card/Cards.jsx
@@ -11,17 +11,17 @@ function Card(props) {
 
       
       <div className="card-body relative   group  bg-slate-100 w-[360px]  h-60 rounded-xl shadow-xl shadow-neutral-300 flex flex-col overflow-hidden duration-500  ">
-        <img src={props.cardPic} alt="" className='h-60  rounded-xl shadow-lg  shadow-neutral-500 group-hover:scale-105 duration-300  md:opacity-100 blur-sm md:blur-none group-hover:blur-sm'/>
-          <div className="demo flex text-center inset-0 group-hover:flex flex-col  justify-center align-middle group-hover:translate-y-[0%]   absolute  md:translate-y-[60%] text-white  md:bg-gradient-to-b from-transparent via-black bg-gradient-to-b   w-[360px] rounded-2xl h-[240px] transition-all  duration-500  " >
+        <img src={props.cardPic} alt="" className='h-60 object-cover rounded-xl shadow-lg  shadow-neutral-500 sm:group-hover:scale-105 duration-300  md:opacity-100  md:blur-none sm:group-hover:blur-sm'/>
+          <div className="demo max-sm:hidden flex text-center inset-0 sm:group-hover:flex flex-col  justify-center align-middle sm:group-hover:translate-y-[0%]   absolute  sm:translate-y-[60%] text-white  md:bg-gradient-to-b from-transparent via-black bg-gradient-to-b   w-[360px] rounded-2xl h-[240px] transition-all  duration-500  " >
             <h1 className='text-2xl  font-bold '>{props.text}</h1>
             <p className='font-bold mt-[10px] '>{props.dtxt}</p>
             <h1 className='text-3xl cursor-pointer ml-[140px] mt-[15px] '> <a href={props.githubLink}><DiGithubBadge size={50} className='hover:scale-110 duration-300'/></a></h1>
             
           </div>
-        {/* <div className="card-text flex justify-center align-middle p-4 ">
+        <div className="card-text flex justify-center sm:hidden align-middle p-4 ">
           <h1 className='text-3xl cursor-pointer'> <a href="https://github.com/Binyam888/masood-protfolio/tree/master"><DiGithubBadge/></a></h1>
           <h1 className=' mt-1 ml-3'>{props.text}</h1>
-        </div> */}
+        </div>
       </div>
     
     
